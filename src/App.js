6f96447a0d020c529818ce787dfd31f8dd5362ce
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2023-03-17 19:09:00 +0100

    ADD: Redirect to Detail + useHistory hook to PetDetailsNotFound Page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
           <Navigation />
         </Route>
           <Switch>
+            <Route path='/pet-details-not-found'>
+              <PetDetailsNotFound />
+            </Route>
             <Route path='/search'>
               <SearchPage />
             </Route>
